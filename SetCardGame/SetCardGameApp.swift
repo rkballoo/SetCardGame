ccdc7f804e71a20a92280b174e7b3f815c533aaf
Rajiv Balloo <rkballoo@gmail.com>
2022-05-19 11:20:58 -0300

    Initial Commit

---
+++ SetCardGame/SetCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCardGameApp.swift
+//  SetCardGame
+//
+//  Created by Rajiv Keshav Balloo on 2022-05-19.
+//
+
+import SwiftUI
+
+@main
+struct SetCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
